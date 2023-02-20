217b8fbc462f0ce486ec2d299f6b8e9b87d63687
tanishqacodes <tanishqasharma797@gmail.com>
2023-02-20 22:10:45 +0530

    This is flask commit

--- app.py
+++ app.py
@@ -1,10 +1,55 @@
 from flask import Flask
+from flask import request,render_template,jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+#present the home page / index page
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+#call maths for opertaion
+@app.route('/math',methods=['POST'])
+def math_ops():
+    if(request.method == 'POST'):
+        ops = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if ops == 'add':
+            r = num1+num2
+            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'subtract':
+            r = num1-num2
+            result = "The subtract of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'multiply':
+            r = num1*num2
+            result = "The multiply of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'divide':
+            r = num1/num2
+            result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+            
+        return render_template('results.html' , result = result)
+#postman used to test api automatically
+@app.route('/postman_action',methods=['POST'])
+def math_ops1():
+    if(request.method == 'POST'):
+        ops = request.json['operation']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if ops == 'add':
+            r = num1+num2
+            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'subtract':
+            r = num1-num2
+            result = "The subtract of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'multiply':
+            r = num1*num2
+            result = "The multiply of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'divide':
+            r = num1/num2
+            result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+            
+        return jsonify(result)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
